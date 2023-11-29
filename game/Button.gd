48eabccf63696a5bfe5ae38c11c0a70fc87da26d
Tani Nevins-Klein <101879650+tnevins-klein@users.noreply.github.com>
2023-11-29 14:52:38 -0500

    test button scene

--- game/Button.gd
+++ game/Button.gd
@@ -1,7 +1,5 @@
-extends Area2D
+extends StaticBody2D
 
-
-var pressed = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -11,3 +9,11 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+func _on_area_2d_body_entered(body):
+	print("pressed")
+	$AnimatedSprite2D.play("pressed")
+
+
+func _on_area_2d_body_exited(body):
+	$AnimatedSprite2D.play("default")
