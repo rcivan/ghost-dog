630c712e9770a75f02bb4d82bf0a13a12b172eb0
Tani Nevins-Klein <101879650+tnevins-klein@users.noreply.github.com>
2023-12-04 15:15:49 -0500

    Collision issues

--- game/elements/stage/Door.gd
+++ game/elements/stage/Door.gd
@@ -5,10 +5,12 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	self.scale.x = 0.8
+	open_door()
 
 func open_door():
 	var tween = get_tree().create_tween()
 	tween.tween_property(self, "scale", Vector2(3,1), 0.5)
+	$DoorSprite/StaticBody2D.collision_mask = 9
 
 func close_door():
 	var tween = get_tree().create_tween()
