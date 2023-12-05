58adea3bef3e78493da5aa6c12eee5fcba3003dd
BlaiseBaptist <bbaptist@thegraysonschool.org>
2023-12-05 11:16:37 -0500

    fixed door

--- game/elements/stage/Door.gd
+++ game/elements/stage/Door.gd
@@ -1,22 +1,23 @@
 extends Node2D
 
-@export var is_open: bool = false
+@export var number: int
 
 # Called when the node enters the scene tree for the first time.
-func _ready():
-	self.scale.x = 0.8
-	open_door()
+
 
 func open_door():
 	var tween = get_tree().create_tween()
 	tween.tween_property(self, "scale", Vector2(3,1), 0.5)
-	$DoorSprite/StaticBody2D.collision_mask = 9
+	get_node("StaticBody2D").set_collision_layer_value(1,false)
+	
 
 func close_door():
 	var tween = get_tree().create_tween()
 	tween.tween_property(self, "scale", Vector2(0.8,1), 0.5)
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+
+
+func _on_static_body_2d_pressed(id):
+	if id == number:
+		open_door()
