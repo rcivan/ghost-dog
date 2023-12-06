53c93f49ceac4c2d98d64590163a57301d66550c
Ghost Dog Fan <ckenney@thegraysonschool.org>
2023-12-06 15:02:21 -0500

    5
    5

--- game/elements/player/Player.gd
+++ game/elements/player/Player.gd
@@ -2,7 +2,7 @@ extends CharacterBody2D
 
 
 const SPEED_CUTOFF = 700.0
-const JUMP_VELOCITY = -1000.0
+const JUMP_VELOCITY = -1050.0
 
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
