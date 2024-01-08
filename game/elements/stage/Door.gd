38f4dfb6560872e97bcec47cc5097f17f2be4cd2
BlaiseBaptist <bbaptist@thegraysonschool.org>
2024-01-08 12:55:58 -0500

    door update

--- game/elements/stage/Door.gd
+++ game/elements/stage/Door.gd
@@ -3,8 +3,9 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 
-@export var delay = 0
-@export var speed = .5
+@export var delay = 0.0
+@export var speed = 0.5
+@export var distance = 6
 var initial_pos
 
 func _ready():
@@ -14,7 +15,7 @@ func open_door():
 	tween.tween_property(
 		self, 
 		"position",
-		initial_pos + (Vector2.UP * 300*self.scale[1]).rotated(self.rotation), 
+		initial_pos + (Vector2.UP * distance*50*self.scale[1]).rotated(self.rotation), 
 		speed
 	)
 	# get_node("DoorSprite/StaticBody2D").set_collision_layer_value(1,false)
