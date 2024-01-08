b75597d65cb80fc61580f788e6d861f0b2b314d6
BlaiseBaptist <bbaptist@thegraysonschool.org>
2024-01-08 14:24:12 -0500

    level door redsign

--- game/elements/stage/Door.gd
+++ game/elements/stage/Door.gd
@@ -5,7 +5,7 @@ extends Node2D
 
 @export var delay = 0.0
 @export var speed = 0.5
-@export var distance = 6
+@export var distance = 60
 var initial_pos
 
 func _ready():
@@ -15,7 +15,7 @@ func open_door():
 	tween.tween_property(
 		self, 
 		"position",
-		initial_pos + (Vector2.UP * distance*50*self.scale[1]).rotated(self.rotation), 
+		initial_pos + (Vector2.UP * distance*50).rotated(self.rotation), 
 		speed
 	)
 	# get_node("DoorSprite/StaticBody2D").set_collision_layer_value(1,false)
