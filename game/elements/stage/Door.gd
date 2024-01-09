3fd3fdea09e119e2771fe1bdb840054d17a3cfcc
BlaiseBaptist <bbaptist@thegraysonschool.org>
2024-01-09 11:19:53 -0500

    door speed change

--- game/elements/stage/Door.gd
+++ game/elements/stage/Door.gd
@@ -4,19 +4,21 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 
 @export var delay = 0.0
-@export var speed = 0.5
+@export var speed = 12.0
 @export var distance = 6
 var initial_pos
 
 func _ready():
 	initial_pos = self.position
+	
+	print(.5)
 func open_door():
 	var tween = create_tween()
 	tween.tween_property(
 		self, 
 		"position",
 		initial_pos + (Vector2.UP * distance*50).rotated(self.rotation), 
-		speed
+		float(distance)/speed
 	)
 	# get_node("DoorSprite/StaticBody2D").set_collision_layer_value(1,false)
 	
@@ -29,7 +31,7 @@ func close_door():
 		self, 
 		"position", 
 		initial_pos , 
-		speed
+		float(distance)/speed
 	)
 	# tween.tween_property(self, "scale", Vector2(0.8,1), 0.5)
 
